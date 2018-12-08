19da45529f31b2e5fd484a738fac4a9f761111c8
Cuong Nguyen <cuongnb14@gmail.com>
2018-12-08 23:16:15 +0700

    add run spider function

--- scrapy_ui/scrapy_ui/crawler/urls.py
+++ scrapy_ui/scrapy_ui/crawler/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add-node', views.add_node, name='add_node'),
     path('nodes/<int:node_id>/<str:project_name>/status', views.status_node, name='status_node'),
+    path('nodes/<int:node_id>/<str:project_name>/add-jobs/<str:spider_name>', views.add_job, name='add_job'),
 ]
